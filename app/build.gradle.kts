ec437a7870286b1c6d4443936304342d887f1cac
Josué <Fimel505011@gmail.com>
2024-05-17 15:40:17 -0600

    Practica Artista Segundo commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.oracle.database.jdbc:ojdbc6:11.2.0.4")
+
 }
---
+++ app/src/main/java/Modelo/ClaseConeion.kt
@@ -0,0 +1,11 @@
+package Modelo
+
+import java.lang.Exception
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClaseConeion {
+
+    
+
+}
